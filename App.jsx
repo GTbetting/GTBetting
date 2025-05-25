fe388714fccf9e11374d5937526c40971a3b7cfd
GTbetting <contato@gtbetting.com.br>
2025-05-25 18:13:00 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>GTBetting Online 🔥</h1>
+      <p>Deploy funcionando com Vite + React!</p>
+    </div>
+  );
+}
